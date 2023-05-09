034cc8b7345ca1ee880fba5a5283dc203c0780a2
Guglielmo Boi <boi.guglielmo@gmail.com>
2023-05-09 22:15:47 +0200

    optimized imports

--- app/src/main/java/com/guglielmoboi/cstimergraph/dialogs/contributors/ContributorsDialog.kt
+++ app/src/main/java/com/guglielmoboi/cstimergraph/dialogs/contributors/ContributorsDialog.kt
@@ -18,7 +18,6 @@
 package com.guglielmoboi.cstimergraph.dialogs.contributors
 
 import android.app.Application
-import android.content.Context
 import android.content.Intent
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
@@ -27,7 +26,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.getColor
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
